c7ba4587bd89cfbb049af7ad4d0c2389938da48d
Nicholas Allee <42816286+jiggensnaggen@users.noreply.github.com>
2020-02-07 13:59:58 -0600

    added the key value pair that dr case wants

--- practiceMapper.py
+++ practiceMapper.py
@@ -1,3 +1,5 @@
 ## this will take in d.txt and map it to another file named mapped.txt
+
+#dr case wants us to map out the category number and the value
 ## mapper ideas
 ## map out store name and payment type
